9e206d1be4e53a1a9b175614576e506044d0ab9a
Edgar Benavides <edgarben27@gmail.com>
2024-11-11 17:44:05 -0600

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update distro
+sudo apt update && sudo apt upgrade -y
+
+# Install essentials tools
+sudo apt install -y wget curl zsh git unzip bat neofetch xclip build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+  libffi-dev liblzma-dev
+
+# Set ZSH as default shell
+read -s -p "Password: " sudo_password
+echo
+echo "$sudo_password" | sudo -S chsh -s "$(which zsh)"
+echo "ZSH set as default shell."
+
+# Install Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
