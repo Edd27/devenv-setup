c04c7717504588b134fae88992f5ca3cdcc9b1a4
Edgar Benavides <edgarben27@gmail.com>
2024-11-11 17:55:06 -0600

    Update 1-update.sh

--- 1-update.sh
+++ 1-update.sh
@@ -8,11 +8,5 @@ sudo apt install -y wget zsh git unzip bat neofetch xclip build-essential libssl
   libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
   libffi-dev liblzma-dev
 
-# Set ZSH as default shell
-read -s -p "Password: " sudo_password
-echo
-echo "$sudo_password" | sudo -S chsh -s "$(which zsh)"
-echo "ZSH set as default shell."
-
 # Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
