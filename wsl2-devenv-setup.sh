2d3f4d6e2e00875c832571879b0c33e9419b1d6b
Edgar Benavides <edgarben27@gmail.com>
2024-11-11 17:35:24 -0600

    Update wsl2-devenv-setup.sh

--- wsl2-devenv-setup.sh
+++ wsl2-devenv-setup.sh
@@ -4,22 +4,6 @@
 mkdir -p ~/dev/magnotechnology
 echo "Directories created successfully."
 
-# Install zsh
-sudo apt install -y zsh
-
-# Prompt user for password
-echo "Please enter your password for sudo commands."
-read -s -p "Password: " sudo_password
-echo
-
-# Use the password with sudo for set ZSH as default Shell
-echo "$sudo_password" | sudo -S chsh -s "$(which zsh)"
-echo "ZSH set as default shell."
-
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Oh My Zsh installed successfully."
-
 # Install ZSH plugins
 ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
