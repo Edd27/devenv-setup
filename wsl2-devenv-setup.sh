c6d01b11849e2ffdd83fa27a710b48f2e6b4ffbb
Edgar Benavides <edgarben27@gmail.com>
2024-11-11 14:11:10 -0600

    feat: Add bash script

---
+++ wsl2-devenv-setup.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Update and install utilities
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y wget curl git unzip bat zsh neofetch xclip build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+    libffi-dev liblzma-dev
+
+# Set ZSH as default shell
+chsh -s $(which zsh)
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Zsh plugins
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
+
+# Update plugins in .zshrc
+sed -i 's/plugins=(.*)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting you-should-use zsh-bat)/' ~/.zshrc
+
+# Restart shell
+exec "$SHELL"
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+# Configure pyenv in .zshrc
+echo -e '\n# pyenv\nexport PYENV_ROOT="$HOME/.pyenv"\n[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init -)"\n# pyenv end\n' >> ~/.zshrc
+
+# Restart shell
+exec "$SHELL"
+
+# Install Python versions using pyenv
+pyenv install 2
+pyenv install 3
+pyenv global 3
+
+# Install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+# Configure pnpm in .zshrc
+echo -e '\n# pnpm\nexport PNPM_HOME="$HOME/.local/share/pnpm"\ncase ":$PATH:" in\n  *":$PNPM_HOME:"*) ;;\n  *) export PATH="$PNPM_HOME:$PATH" ;;\nesac\n# pnpm end\n' >> ~/.zshrc
+
+# Restart shell
+exec "$SHELL"
+
+# Set Node.js version with pnpm
+pnpm -g env use 18
+
+# Install corepack globally
+pnpm add -g corepack
+
+# Create global .gitignore
+touch ~/.gitignore
+
+# Set Git global configurations
+git config --global user.name "Edgar Benavides"
+git config --global user.email "edgarben27@gmail.com"
+git config --global core.editor "code --wait"
+git config --global core.autocrlf input
+git config --global init.defaultbranch main
+git config --global core.fileMode false
+git config --global core.excludesfile ~/.gitignore
+
+# Create SSH directory and generate key
+mkdir -p ~/.ssh
+cd ~/.ssh
+ssh-keygen -t ed25519 -b 4096 -C "edgarben27@gmail.com" -f GitHub_Edd27
+
+# Initialize SSH agent and add key
+eval "$(ssh-agent -s)"
+ssh-add GitHub_Edd27
+
+# Add SSH config for GitHub
+cat <<EOL > ~/.ssh/config
+# Personal Github
+Host github.com
+  HostName github.com
+  PreferredAuthentications publickey
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/GitHub_Edd27
+EOL
+
+# Suppress login messages
+touch ~/.hushlogin
+
+# Add aliases to .zshrc
+cat <<EOL >> ~/.zshrc
+
+# Custom aliases
+alias zshconfig="code ~/.zshrc"
+alias ohmyzsh="code ~/.oh-my-zsh"
+alias pn="pnpm"
+alias home="cd ~"
+alias dev="cd ~/dev && pn -g env use 18"
+alias gpm="gp origin main"
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias glgm="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --author='edgarben27@gmail.com'"
+alias rclaro="cd ~/dev/magnotechnology/render-claro-co && pyenv shell 2 && code ."
+alias apiclaro="cd ~/dev/magnotechnology/api-claro-co && pyenv shell 2 && code ."
+EOL
+
+# Final shell restart to apply changes
+exec "$SHELL"
+
+echo "Environment setup completed!"
