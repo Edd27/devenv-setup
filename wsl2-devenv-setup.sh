8ab68edaa0809e551a60f31d73f9d896a22d854f
Edgar Benavides <edgarben27@gmail.com>
2024-11-11 16:03:30 -0600

    chore: Update script

--- wsl2-devenv-setup.sh
+++ wsl2-devenv-setup.sh
@@ -2,20 +2,25 @@
 
 # Create work directories if they do not exist
 mkdir -p ~/dev/magnotechnology
+echo "Directories created successfully."
+
 
 # Update and install utilities
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y wget curl git unzip bat zsh neofetch xclip build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
     libffi-dev liblzma-dev
+echo "Utilities installed successfully."
 
 # Set ZSH as default shell
 chsh -s $(which zsh)
+echo "ZSH set as default shell."
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Oh My Zsh installed successfully."
 
-# Install Zsh plugins
+# Install ZSH plugins
 ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
@@ -24,6 +29,7 @@ git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
 
 # Update plugins in .zshrc
 sed -i 's/plugins=(.*)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting you-should-use zsh-bat)/' ~/.zshrc
+echo "ZSH plugins updated successfully."
 
 # Restart shell
 exec "$SHELL"
@@ -33,6 +39,7 @@ curl https://pyenv.run | bash
 
 # Configure pyenv in .zshrc
 echo -e '\n# pyenv\nexport PYENV_ROOT="$HOME/.pyenv"\n[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init -)"\n# pyenv end\n' >> ~/.zshrc
+echo "pyenv configured successfully."
 
 # Restart shell
 exec "$SHELL"
@@ -41,6 +48,7 @@ exec "$SHELL"
 pyenv install 2
 pyenv install 3
 pyenv global 3
+echo "Python versions installed successfully."
 
 # Install pnpm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
@@ -56,6 +64,7 @@ pnpm -g env use 18
 
 # Install corepack globally
 pnpm add -g corepack
+echo "pnpm installed successfully."
 
 # Create global .gitignore
 touch ~/.gitignore
@@ -78,7 +87,7 @@ ssh-keygen -t ed25519 -b 4096 -C "edgarben27@gmail.com" -f GitHub_Edd27
 eval "$(ssh-agent -s)"
 ssh-add GitHub_Edd27
 
-# Add SSH config for GitHub
+# Add SSH config for GitHub Account
 cat <<EOL > ~/.ssh/config
 # Personal Github
 Host github.com
@@ -87,6 +96,22 @@ Host github.com
   AddKeysToAgent yes
   IdentityFile ~/.ssh/GitHub_Edd27
 EOL
+
+# Copy the new SSH key to clipboard
+xclip -selection clipboard < GitHub_Edd27.pub
+echo "The SSH public key has been copied to your clipboard."
+
+# Prompt user to confirm if they have added the SSH key to their GitHub account
+read -p "Have you added the SSH key to your GitHub account? (yes/no): " ssh_added
+
+if [[ "$ssh_added" == "yes" ]]; then
+  # Test SSH connection to GitHub
+  ssh -T git@github.com
+else
+  echo "Skipping SSH connection test. Please remember to test your SSH connection after adding the key."
+fi
+
+echo "Git configurations set successfully."
 
 # Suppress login messages
 touch ~/.hushlogin
@@ -106,6 +131,8 @@ alias glgm="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset
 alias rclaro="cd ~/dev/magnotechnology/render-claro-co && pyenv shell 2 && code ."
 alias apiclaro="cd ~/dev/magnotechnology/api-claro-co && pyenv shell 2 && code ."
 EOL
+
+echo "Aliases added successfully."
 
 # Final shell restart to apply changes
 exec "$SHELL"
