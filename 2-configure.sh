3b87218e97fb5e90bde9258197ccd73fffefd6ea
Edgar Benavides <edgarben27@gmail.com>
2024-11-14 11:48:12 -0600

    Update 2-configure.sh

--- 2-configure.sh
+++ 2-configure.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+# Create work directories if they do not exist
+mkdir -p ~/dev/magnotechnology
+echo "✔️ Work directories created successfully."
+echo
+
 # Install ZSH plugins
 ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions" > /dev/null 2>&1
@@ -68,11 +73,6 @@ echo "✔️ Gitignore file created and populated."
 echo
 
 touch ~/.hushlogin
-
-# Create work directories if they do not exist
-mkdir -p ~/dev/magnotechnology
-echo "✔️ Work directories created successfully."
-echo
 
 # Create configuration file for erdtree
 cat <<EOL > ~/.erdtreerc
