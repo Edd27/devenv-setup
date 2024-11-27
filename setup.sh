743e5e8b05e12ed32d4df3d4e6a12f1bba2fbdc3
Edgar Benavides <edgarben27@gmail.com>
2024-11-26 18:22:50 -0600

    fix: Replacing fnm env on zshrc file

--- setup.sh
+++ setup.sh
@@ -289,12 +289,12 @@ if [[ "$os_type" == "Darwin" ]]; then
     sed -i '' 's/^#\(export ZSH="\$HOME\/.oh-my-zsh"\)/\1/' "$ZSHRC_FILE"
     sed -i '' 's/^#\(ZSH_THEME="robbyrussell"\)/\1/' "$ZSHRC_FILE"
     sed -i '' 's/^#\(source "\$ZSH\/oh-my-zsh.sh"\)/\1/' "$ZSHRC_FILE"
-    sed -i '' 's/^eval \`fnm env .*\`/eval \`fnm env --use-on-cd --version-file-strategy=recursive --shell zsh\`/' "$ZSHRC_FILE"
+    sed -i '' 's/^eval "\`fnm env .*\`"/eval \`fnm env --use-on-cd --version-file-strategy=recursive --shell zsh\`/' "$ZSHRC_FILE"
 else
     sed -i 's/^#\(export ZSH="\$HOME\/.oh-my-zsh"\)/\1/' "$ZSHRC_FILE"
     sed -i 's/^#\(ZSH_THEME="robbyrussell"\)/\1/' "$ZSHRC_FILE"
     sed -i 's/^#\(source "\$ZSH\/oh-my-zsh.sh"\)/\1/' "$ZSHRC_FILE"
-    sed -i 's/^eval \`fnm env .*\`/eval \`fnm env --use-on-cd --version-file-strategy=recursive --shell zsh\`/' "$ZSHRC_FILE"
+    sed -i 's/^eval "\`fnm env .*\`"/eval \`fnm env --use-on-cd --version-file-strategy=recursive --shell zsh\`/' "$ZSHRC_FILE"
 fi
 
 cd ~
