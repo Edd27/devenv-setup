1b6524b6ed928c2cfb7da915bd76c97a501d9429
Edgar Benavides <edgarben27@gmail.com>
2025-11-27 20:58:06 -0600

    Remove 'fastfetch' from package installation list
    Removed 'fastfetch' from the list of packages to install.

--- setup.sh
+++ setup.sh
@@ -246,7 +246,7 @@ install_essentials() {
     sudo apt upgrade -y &>>"$LOG_FILE" || warning "Some packages failed to upgrade"
 
     local packages=(
-        wget curl git unzip bat fastfetch xclip make build-essential
+        wget curl git unzip bat xclip make build-essential
         libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
         libncursesw5-dev libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev
         libffi-dev liblzma-dev libgdbm-dev libnss3-dev libexpat1-dev
